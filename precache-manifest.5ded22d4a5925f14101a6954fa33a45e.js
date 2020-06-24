e306946970278768e0143b47565d0c498fa0686d
pepeyen <erickfcaldeira@gmail.com>
2020-06-24 10:55:27 -0300

    Updates

---
+++ precache-manifest.5ded22d4a5925f14101a6954fa33a45e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79e27bc06048388945b51bbf95516b3",
+    "url": "/cs1-basics/index.html"
+  },
+  {
+    "revision": "fbe57e551fbc1213bc34",
+    "url": "/cs1-basics/static/css/2.f6037f30.chunk.css"
+  },
+  {
+    "revision": "39d16d374eb48df54dd1",
+    "url": "/cs1-basics/static/css/main.c71c3063.chunk.css"
+  },
+  {
+    "revision": "fbe57e551fbc1213bc34",
+    "url": "/cs1-basics/static/js/2.7b3379a6.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/cs1-basics/static/js/2.7b3379a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d16d374eb48df54dd1",
+    "url": "/cs1-basics/static/js/main.c53539f6.chunk.js"
+  },
+  {
+    "revision": "5e74501d0046dfe52787",
+    "url": "/cs1-basics/static/js/runtime-main.89e0dd67.js"
+  },
+  {
+    "revision": "08d41ba999899cc916fb8a08d6bba214",
+    "url": "/cs1-basics/static/media/FontExo2Bold.08d41ba9.woff"
+  },
+  {
+    "revision": "0fdd5b581c8877d8cace89886dd35c57",
+    "url": "/cs1-basics/static/media/FontExo2ExtLight.0fdd5b58.woff"
+  },
+  {
+    "revision": "b51b0b0ce8fe1105e65732b235b5f115",
+    "url": "/cs1-basics/static/media/FontExo2Light.b51b0b0c.woff"
+  },
+  {
+    "revision": "62b76bfa0bed1a1db15eb34814548380",
+    "url": "/cs1-basics/static/media/FontExo2Medium.62b76bfa.woff"
+  },
+  {
+    "revision": "129082a0a23d95c0a240dc56c589694d",
+    "url": "/cs1-basics/static/media/FontExo2Thin.129082a0.woff"
+  },
+  {
+    "revision": "74735d14cafedb546ae885e7d603323c",
+    "url": "/cs1-basics/static/media/cart.74735d14.svg"
+  },
+  {
+    "revision": "5480beaa53b5dd49f1f5879ff9148933",
+    "url": "/cs1-basics/static/media/html5.5480beaa.png"
+  },
+  {
+    "revision": "bdaf2b3210ec5bc9dd5f1a4a23aa81b3",
+    "url": "/cs1-basics/static/media/jsf-faded.bdaf2b32.png"
+  },
+  {
+    "revision": "7fdf4443a5fe9dd88b007b265f5f931d",
+    "url": "/cs1-basics/static/media/jsf.7fdf4443.png"
+  },
+  {
+    "revision": "ea340ff856da8e47f038617164923cf4",
+    "url": "/cs1-basics/static/media/php-faded.ea340ff8.png"
+  },
+  {
+    "revision": "b4a23ad1cf0a76e641660126e4b3259e",
+    "url": "/cs1-basics/static/media/php.b4a23ad1.png"
+  },
+  {
+    "revision": "716b77d1b10f71528657d565e42759d7",
+    "url": "/cs1-basics/static/media/reactjs-faded.716b77d1.png"
+  },
+  {
+    "revision": "394b399c50ec05a7ea4feaac2c070d55",
+    "url": "/cs1-basics/static/media/reactjs.394b399c.png"
+  }
+]);
